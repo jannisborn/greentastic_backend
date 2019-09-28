3c83fd29f2755c7d724af6f255d3564c141fa640
NinaWie <ninawiedemann999@gmail.com>
2019-09-28 15:39:08 +0200

    sublists of dist and dur included in score computation

--- compute_scores.py
+++ compute_scores.py
@@ -3,35 +3,89 @@
 
 import numpy as np
 import json
+from backend import get_directions
 
-def compute_score(info_dic, out_dic, weights = [1,1,1,1], transport_list=["car","bike", "walk"]):
-    norm_weights = weights/np.sum(weights)
-
-    # check: duration in minutes, distances in km
-    value_arr = np.zeros((len(out_dic.keys()), 4))
-    for i, transport in enumerate(sorted(out_dic.keys())):
-        d = out_dic[transport]["distance"]*0.001 # distance
-        m = out_dic[transport]["duration"] # duration
-        infos = info_dic[transport]
-        e = infos["emissionsProKM"]*d # emissions
-        if "price_fahrt" in infos:
-            p = infos["price_fahrt"] # price
-        elif "priceKm" in infos:
-            p = infos["priceKm"]*d
-        else:
-            p = infos["priceProMin"]*m
-        c = infos["caloriesPerMin"]*m # calories
-        value_arr[i] = [m,e,p,c]
-        out_dic[transport] = {"emission": round(e,2), "calories":round(c,2), "price":round(p,2), "distance":round(d,2), "duration":round(m,2)}
+def normalize_value_arr(value_arr):
     mins = np.amin(value_arr, axis=0)
     maxs = np.amax(value_arr, axis=0)
     norm_value_arr = 1-(value_arr-mins)/(maxs-mins) # normalize and invert at the same time, i.e. high score is good
     norm_value_arr[:,3] = 1-norm_value_arr[:,3] # calories needs to be inverted
     norm_value_arr = np.around(norm_value_arr, 2)
+    return norm_value_arr
+
+def compute_score(info_dic, maps_dic, weights = [1,1,1,1]):
+    norm_weights = weights/np.sum(weights)
+    out_dic = {}
+    # check: duration in minutes, distances in km
+    nonempty_keys = [] # if no car route can be found, then it is empty
+    value_arr = [] # np.zeros((len(info_dic.keys()), 4))
+    for i, transport in enumerate(sorted(info_dic.keys())):
+        out_dic[transport] = {}
+        # print("\n TRANSPORT:", transport)
+        maps_key = info_dic[transport]["maps_key"] # current corresponding key, eg transit
+
+        # dist_dic = maps_dic[maps_key]["distance"]
+        # dur_dic = maps_dic[maps_key]["duration"]
+        # print(maps_dic.keys(),maps_key, maps_dic[maps_key])
+        dist_dic = maps_dic.get(maps_key, {}).get("distance", {})
+        dur_dic = maps_dic.get(maps_key, {}).get("duration", {})
+
+
+        if not dist_dic:
+            for k in ["emission", "calories", "price", "distance", "duration", "total_weighted_score",
+            "total_weighted_score_col", "duration_score", "emission_score", "price_score", "calories_score",
+            "duration_score", "emission_score", "price_score", "calories_score"]:
+                out_dic[transport][k] = 0
+            out_dic[transport]["coordinates"]=[]
+            continue
+
+        total_duration = round(sum(dur_dic.values()),2) # duration is sum of all durations
+        total_distance = round(sum(dist_dic.values()),2)
+
+        # one list for each part of the way
+        prices = []
+        emissions = []
+        calories = []
+        for j, step_key in enumerate(sorted(dist_dic.keys())): # distances
+            # for each part of the way, compute the absolute amount of calories burnt,
+            # the price and the absolute amount of emissions
+            if step_key==maps_key:
+                step_transport = transport # wenn key in dist dic "cycling" ist dann ist das "escooter"
+            else:
+                step_transport = step_key
+            # print("Step: ", step_transport)
+            d = dist_dic[step_key]*0.001 # distance of this step per KM
+            m = dur_dic[step_key]/60 # duration of this step per MIN
+            infos = info_dic[step_transport]
+
+            emissions.append(infos["emissionsProKM"]*d) # emissions
+            if "price_fahrt" in infos:
+                p = infos["price_fahrt"]
+            elif "priceKm" in infos:
+                p = infos["priceKm"]*d
+            else:
+                p = infos["priceMin"]*m
+            prices.append(p) # price
+            calories.append(infos["caloriesPerMin"]*m) # calories
+            # print("e:", emissions, "p:", prices, "c:", calories)
+
+        total_emissions = round(sum(emissions),2)
+        total_calories = round(sum(calories),2)
+        total_price = round(sum(prices),2)
+
+        nonempty_keys.append(transport)
+        value_arr.append([total_duration, total_emissions, total_price, total_calories])
+
+        out_dic[transport] = {"emission": total_emissions, "calories":total_calories, "price":total_price, "distance":total_distance, "duration":total_duration}
+        # out_dic[transport]["coordinates"] = maps_dic.get(maps_key,{}).get("coordinates",[])
+
+    value_arr = np.asarray(value_arr)
+    norm_value_arr = normalize_value_arr(value_arr)
+    # print(np.around(norm_value_arr,2))
 
     colours = [[245, 53, 53], [255, 128,0], [255,255,0],[153,255,51], [0,153,0]]
     colour_scores = [1,3,5,7,9]
-    for i, transport in enumerate(sorted(out_dic.keys())):
+    for i, transport in enumerate(nonempty_keys):
         for j, score_name in enumerate(["duration_score", "emission_score", "price_score", "calories_score"]):
             out_dic[transport][score_name] = norm_value_arr[i,j]
         for j, score_name in enumerate(["duration_col", "emission_col", "price_col", "calories_col"]):
@@ -47,6 +101,14 @@ def compute_score(info_dic, out_dic, weights = [1,1,1,1], transport_list=["car",
 if __name__=="__main__":
     with open("metadata.json", "r") as infile:
         dic = json.load(infile)
-    out_dic = compute_score(dic, {"car":{"duration":5, "distance":1000}, "walk":{"duration":10, "distance":1100},
-                              "bike":{"duration":3, "distance":700}}, weights=[1,1,1,1])
+    maps_dic = get_directions((47.3857, 8.5668), (47.3649, 8.5469)) #(47.3495, 8.4920)) #
+    # print(maps_dic)
+    out_dic = compute_score(dic, maps_dic, weights=[1,1,1,1])
+    # out_dic = compute_score(dic, {"car":{"duration":5, "distance":1000}, "walk":{"duration":10, "distance":1100},
+                             #  "bike":{"duration":3, "distance":700}}, weights=[1,1,1,1])
     print(out_dic)
+    for key in out_dic.keys():
+        print(key, out_dic[key]["total_weighted_score"])
+
+    with open("example_output.json", "w") as outfile:
+        json.dump(out_dic, outfile)
