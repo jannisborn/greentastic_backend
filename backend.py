d747055ffac2439ab2d7076890c809d2511817b9
jannisborn <jannis.born@gmx.de>
2019-09-28 13:09:54 +0200

    Documentation to query directions

--- backend.py
+++ backend.py
@@ -13,44 +13,46 @@ API_KEY = open(os.getcwd() + '/clean_commuter.keypair', 'r').read()
 GMAPS = GoogleMaps(API_KEY)
 
 
-def get_directions(query):
+def get_directions(start, end):
+    """
+    Query distances API from Google Maps to compute route from start to end.
 
-    # start = query[0]
-    # end = query[1]
+    Args:
+        - START {string, dict, list tuple}. E.g. a string of a place or its GPS coordinates.
+        - END {string, dict, list tuple}. E.g. a string of a place or its GPS coordinates.
 
-    start = "Zürich Hauptbahnhof, Bahnhofpl., 8001 Zürich"
-    end = "ETH Zürich Hauptgebäude, Rämistrasse 101, 8092 Zürich"
+    Returns:
+        - DIRECTIONS {dict}.    Keys are transportation modes, values are:
+                                    - distance: a dict consisting of modes and distances.
+                                    - duration: a dict consisting of modes and distances.
+                                    - coordinates: a list of GPS coordinates.
+    """
 
-    response = dict()
+    directions = dict()
 
-    #for mode in ["driving", "walking", "bicycling", "transit"]:
-    for mode in ["driving"]:
-
-        directions = GMAPS.directions(start,
-                                      end,
-                                      mode=mode,
-                                      departure_time=datetime.now())
+    for mode in ["driving", "walking", "bicycling", "transit"]:
+        routes = GMAPS.directions(start,
+                                  end,
+                                  mode=mode,
+                                  departure_time=datetime.now())
 
         # Allocate dict
-        response[mode] = dict()
-        response[mode]['distance'] = {mode: 0, "walking": 0}
-        response[mode]['duration'] = {mode: 0, "walking": 0}
-        response[mode]['coordinates'] = []
+        directions[mode] = dict()
+        directions[mode]['distance'] = {mode: 0, "walking": 0}
+        directions[mode]['duration'] = {mode: 0, "walking": 0}
+        directions[mode]['coordinates'] = []
 
         # Gather data (parse individual parts of the journey)
-        for step in directions[0]['legs'][0]['steps']:
+        for step in routes[0]['legs'][0]['steps']:
 
             # Parse the polyline into GPS coordinates
-            response[mode]['coordinates'].extend(
+            directions[mode]['coordinates'].extend(
                 polyline.decode(step['polyline']['points']))
 
             # Update the distance and duration values
-            response[mode]['duration'][
+            directions[mode]['duration'][
                 step['travel_mode'].lower()] += step['duration']['value']
-            response[mode]['distance'][
+            directions[mode]['distance'][
                 step['travel_mode'].lower()] += step['distance']['value']
 
-    return response
-
-
-#%%
+    return directions
